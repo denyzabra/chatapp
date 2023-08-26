fe2bdd8e7815187a5c802591ebb0f149f66e9055
denyzabra <denyzabrahams0@gmail.com>
2023-08-26 12:43:56 +0300

    Correct placement of userAuthenticated event

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ app.listen(PORT, () => {
 });
 */
 
+
+/*
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -118,3 +120,81 @@ app.get('/',(req, res) => {
 
 //display username in chat
 socket.emit('userAuthenticated', { username });
+*/
+
+//modified code
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define your routes here
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Send the HTML file
+});
+
+// Handle Socket.io events
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
+    socket.on('message', (message) => {
+        io.emit('message', message); // Emit the received message to all clients
+    });
+
+    // Emit the 'userAuthenticated' event when a user is authenticated
+    socket.emit('userAuthenticated', { username }); // Correct placement here
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+//added a route for login page
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public','login.html'));
+});
+
+//user authentication
+const users =[
+    {username: 'user1', password: 'password1'},
+    {username:'user2',password: 'password2'},
+    //add more users as needed
+];
+
+//handle login POST request
+app.post('/login', (req, res) => {
+    const{username, password} = req.body;
+
+    const user = users.find(u => u.username === username && u.password === password);
+    if(user){
+        //Redirect to chat interface or another page
+        res.redirect('/');
+    } else{
+        //Redirect back to login page with an error message
+        res.redirect('/login?error=true');
+    }
+    
+});
+
+//restrict access to chat interface
+app.get('/',(req, res) => {
+    //check if user is authenticated,otherwise redirect to login page
+    if(req.isAuthenticated()){
+        res.sendFile(path.join(__dirname,'public','index.html'));
+
+    }else{
+        res.redirect('/login');
+    }
+});
